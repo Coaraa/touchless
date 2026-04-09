b983089e7de1def881cfac05f3982b850c96a68e
Florian Touraine <128731709+Styleflo@users.noreply.github.com>
2026-04-09 14:07:17 -0400

    Ajout de l'API

---
+++ API/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import sys
+import os
+import subprocess
+
+# BASE_DIR sera le dossier 'py_scripts/static_model/'
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(CURRENT_DIR)
+PYSCRIPT_DIR = os.path.join(BASE_DIR, "py_scripts")
+
+app = FastAPI()
+
+@app.get("/")
+async def Welcome():
+    return {"message": "Hello from FastAPI!"}
+
+@app.get("/run/static")
+def run():
+
+    script_path = os.path.join(PYSCRIPT_DIR, "static_model/model_run.py")
+    subprocess.Popen([sys.executable, script_path])
+
+    return {"message": "Lancement du modele statique !"}
+
+
